b8fd4b7885c05dfbfaeff7ac40bb51db44e095c9
Ryuya Ikeda <rikeda71@gmail.com>
2017-03-30 23:54:27 +0900

    03/30 commit

--- 05/49.py
+++ 05/49.py
@@ -2,30 +2,48 @@
 import re
 import readfile
 
-#名詞間への係り受けパスを出力するメソッド
-def out_n_to_n(chunk,i) -> str:
-    # X の名詞文字列を作成
-    noun = ''
-    for j in range(len(chunk[i].morphs)):
-        if chunk[i].morphs[j].pos in '名詞':
-            noun += noun + 'X'
-        else:
-            noun += chunk[i].morphs[j].surface
-    # Y以降の文字列を作成
-    lines = []
-    # ペアを探す
+def one_n_to_n(chunk,i,noun) -> str:
     for j in range(i,len(chunk)):
-        line = noun
-        noun_flag = False
-        # 1つの組み合わせを作る
+        line += noun
+        if chunk[i].dst == j+1
+            line += '->'
+        else:
+            line += '|'
+        for k in range(j+1,len(chunk[j])):
+            line += chunk[j].morphs[k]
+
+
+#名詞間への係り受けパスを出力するメソッド
+def out_n_to_n(chunk) -> str:
+    noun = ''
+    lines = []
+    for j in range(len(chunk)):
+        # 名詞がないなら無視する
+        # 一番端でも無視する
+        if not chunk[j].bool_in_speech('名詞')\
+                or j+1 == len(chunk):
+            continue
+        # X の名詞文字列を作成
         for k in range(len(chunk[j].morphs)):
-            if chunk[j].morphs[k].pos in '名詞' and not noun_flag:
-                noun_flag = True
-                line += 'Y'
+            if chunk[j].morphs[k].pos in '名詞':
+                noun += noun + 'X'
             else:
-                line += chunk[j].morphs[k].surface
+                noun += chunk[j].morphs[k].surface
+        # Y以降の文字列を作成
+        lines.append(one_n_to_n(chunk,j+1,noun))
+        # ペアを探す
+        for j in range(i,len(chunk)):
+            line = noun
+            noun_flag = False
+            # 1つの組み合わせを作る
+            for k in range(len(chunk[j].morphs)):
+                if chunk[j].morphs[k].pos in '名詞' and not noun_flag:
+                    noun_flag = True
+                    line += 'Y'
+                else:
+                    line += chunk[j].morphs[k].surface
             # 構文木の経路上かどうか
-            if chunk[j].morphs[k].dst == j+1
+            if chunk[j].dst == j+1:
                 line += '->'
             else:
                 line += '|'
@@ -34,16 +52,14 @@ def out_n_to_n(chunk,i) -> str:
                 違いは、｜がない場合に、XとYが揃えばそく終了というところ？？
                 どう区別するかが難しいorz
                 """
-        lines.append(line)
+            lines.append(line)
+    return '\n'.join(lines)
 
 if __name__ == '__main__':
     sentences = readfile.read_chunks('neko.txt.cabocha')
     out_line = ''
     dis = [] # 助詞の位置
     for i in range(len(sentences)):
-        for j in range(len(sentences[i])):
-            # 名詞から始まるならパスを追加
-            if sentences[i][j].bool_in_speech('名詞'):
                 out_line += out_n_to_n(sentences[i],j) 
 
     with open('out49.txt','w') as f:
