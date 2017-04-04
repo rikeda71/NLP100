1eaead1aebb49a08715322b44be0d89ba044bd2e
Ryuya Ikeda <rikeda71@gmail.com>
2017-04-04 23:51:31 +0900

    edit 53.py

--- 06/53.py
+++ 06/53.py
@@ -1,15 +1,9 @@
-import xml
+import xml.etree.ElementTree as ET
 import corenlp
-import json
-import pprint
 
 if __name__ == '__main__':
-    # パーサの生成
-    corenlp_dir = "/usr/local/lib/stanford-corenlp-full-2015-04-20/"
-    parser = corenlp.StanfordCoreNLP(corenlp_path=corenlp_dir)
-
-    with open('nlp.txt','r') as f:
-        file_sentence = f.read()
-    # パースして結果をpretty print
-    result_json = json.loads(parser.parse(file_sentence))
-    pprint.pprint(result_json)
+    tree = ET.parse('nlp.txt.xml')
+    root = tree.getroot()
+    for tree in root[0][0][0]:
+        for word in tree.iter('word'):
+            print(word.text)
