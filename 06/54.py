d9909988968789a9ab69fbee5527f021b4e1ae54
Ryuya Ikeda <s14t284@stmail.eng.kagawa-u.ac.jp>
2017-04-05 19:23:03 +0900

    add q54,55

--- 06/54.py
+++ 06/54.py
@@ -3,5 +3,5 @@ import xml.etree.ElementTree as ET
 if __name__ == '__main__':
     tree = ET.parse('nlp.txt.xml')
     root = tree.getroot()
-    for word in root.iter('word'):
-        print(word.text)
+    for token in root.iter('token'):
+        print('%s\t%s\t%s'%(token[0].text,token[1].text,token[4].text))
--- 06/55.py
+++ 06/55.py
@@ -3,5 +3,6 @@ import xml.etree.ElementTree as ET
 if __name__ == '__main__':
     tree = ET.parse('nlp.txt.xml')
     root = tree.getroot()
-    for word in root.iter('word'):
-        print(word.text)
+    for token in root.iter('token'):
+      if token[5].text == 'PERSON':
+          print(token[0].text)
