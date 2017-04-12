d5940ff7b1669145498f733b61c46cda8c9c04b7
Ryuya Ikeda <s14t284@stmail.eng.kagawa-u.ac.jp>
2017-04-12 20:23:13 +0900

    add q70

---
+++ 08/70.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+import codecs
+import random
+
+# 指定したタグを文頭に着けてリストで返す
+def add_tag(filename, tag) -> list:
+    lines = []
+    # タグ付けしたテキストを用意
+    with codecs.open(filename, 'r', 'cp1252') as f:
+        for line in f.readlines():
+            lines.append(tag + line)
+
+    return lines
+
+if __name__ == '__main__':
+    # ポジネガのタグをつける
+    pos = add_tag('rt-polaritydata/rt-polarity.pos', '+1')
+    neg = add_tag('rt-polaritydata/rt-polarity.neg', '-1')
+    # 結合してシャッフル
+    concatenate = pos
+    concatenate.extend(neg)
+    random.shuffle(concatenate)
+    # 書き込み
+    with open('sentiment.txt','w') as f:
+        f.write(''.join(concatenate))
+
