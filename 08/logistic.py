70e9730403d819015aab8b1771112b57eff01d40
Ryuya Ikeda <rikeda71@gmail.com>
2017-07-02 17:49:55 +0900

    Edit Logystic Regression Model but not complete

--- 08/logistic.py
+++ 08/logistic.py
@@ -14,34 +14,33 @@ class Logistic_Regression:
     def sigmoid(self, z):
         return 1.0 / (1 + np.exp(-z))
 
-    def learn(self):
-        np.random.seed()
-        self.w = [0] * len(self.vector[0])
-        learn_rate = 0.1
-        while True:
+    def learn(self, val=100):
+        # np.random.seed()
+        self.w = np.array([0.0] * len(self.vector[0]))
+        eta = 0.1
+        for i in range(1, val + 1):
             a = 0
             old_w = self.w
             for i in range(len(self.sign)):
-                y = self.sigmoid(np.inner(self.vector[i], old_w))
-                self.w -= learn_rate * (y - self.sign[i]) * self.vector[i]
+                y = self.sigmoid(np.inner(old_w.T, self.vector[i]))
+                self.w -= eta * (y - self.sign[i]) * self.vector[i]
             for i in range(len(self.w)):
                 a += np.abs(self.w[i] - old_w[i])
-            learn_rate += 0.9
-            if(np.all(self.w == old_w)):
-                break
+            eta *= 0.9
+        print(self.w)
 
     def get_pos_prob(self, vector: list):
         prob = 0
         for i in range(len(vector)):
-            prob = vector[i] * self.w[i]
-        return prob
+            prob += vector[i] * self.w[i]
+        return self.sigmoid(prob)
 
     def get_neg_prob(self, vector: list):
         prob = 0
-        idx = len(self.w) / 2
+        idx = int(len(self.w) / 2)
         for i in range(len(vector)):
-            prob = vector[i] * self.w[i]
-        return prob
+            prob += vector[i] * self.w[idx + i]
+        return self.sigmoid(prob)
 
     def get_learn_data(self) -> list:
         return self.w.tolist()
--- 08/q73.py
+++ 08/q73.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import re
 from logistic import Logistic_Regression
+from nltk import PorterStemmer
 
 
 def get_sentence_vector(sentence: str, features: list) -> list:
@@ -8,27 +9,42 @@ def get_sentence_vector(sentence: str, features: list) -> list:
     文章のベクトルを返す
     """
 
-    value = len(features)
-    sign = int(sentence[:2])
     vector = []
+    stemmer = PorterStemmer()
+    stem_words = []
 
     # 単語分割
     words = re.split(r"[,.:;\s]", sentence[2:])
     while words.count("") > 0:
         words.remove("")
+    for word in words:
+        stem_words.append(stemmer.stem(word))
 
     # ベクトル作成
     for i in range(len(features)):
-        if features[i][:-1] in words:
+        if features[i][:-1] in stem_words:
             vector.append(1.0)
         else:
             vector.append(0.0)
 
+    return vector
+
+
+def get_sentence_learn_vector(sentence: str, features: list) -> list:
+    """
+    学習のための文章のベクトルを返す
+    """
+    sign = int(sentence[:2])
+    value = len(features)
+    vector = get_sentence_vector(sentence, features)
+
     # 文章の極性によってベクトルの位置を変更
     if sign == 1:
-        return vector + [0] * value + [1.0]
+        # return vector + [0] * value + [1.0]
+        return vector + [0] * value
     else:
-        return [0] * value + vector + [1.0]
+        # return [0] * value + vector + [1.0]
+        return [0] * value + vector
 
 
 if __name__ == "__main__":
@@ -41,7 +57,7 @@ if __name__ == "__main__":
 
     with open("sentiment.txt", "r") as f:
         for sentence in f.readlines():
-            vectors.append(get_sentence_vector(sentence, features))
+            vectors.append(get_sentence_learn_vector(sentence, features))
             if sentence[:2] == "+1":
                 sign.append(1.0)
             else:
@@ -52,3 +68,7 @@ if __name__ == "__main__":
     with open("learndata.txt", "w") as f:
         data = map(str, learn_circuit.get_learn_data())
         f.write("\n".join(data))
+    with open("sentiment.txt", "r") as f:
+        vec = get_sentence_vector(f.readlines()[23], features)
+        print(learn_circuit.get_pos_prob(vec))
+        print(learn_circuit.get_neg_prob(vec))
