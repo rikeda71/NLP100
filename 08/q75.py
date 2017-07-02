78409eea499d5f1ed623883a9f650398d451b037
Ryuya Ikeda <rikeda71@gmail.com>
2017-07-03 00:24:03 +0900

    Add q75

---
+++ 08/q75.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+
+def main():
+    # 素性の格納
+    features = []
+    with open("features.txt", "r") as f:
+        for feature in f.readlines():
+            features.append(feature)
+
+    # 学習データの格納
+    learn_data = []
+    with open("learndata.txt", "r") as f:
+        for val in f.readlines():
+            learn_data.append(float(val))
+
+    # 素性と学習結果の紐づけ
+    feature_dict = {}
+    list_length = len(features)
+    for i in range(list_length):
+        feature_dict[features[i]] = learn_data[i] + learn_data[i + list_length]
+
+    # ソート
+    sort_dict = sorted(feature_dict.items(), key=lambda x: x[1])
+
+    # 重みの高い素性Top10
+    dict_length = len(sort_dict)
+    for i in range(dict_length - 1, dict_length - 11, -1):
+        dic = sort_dict[i]
+        print(str(dic[0][:-1]) + "\t" + str(dic[1]))
+
+    # 重みの低い素性Top10
+    for i in range(10):
+        dic = sort_dict[i]
+        print(str(dic[0][:-1]) + "\t" + str(dic[1]))
+
+
+if __name__ == "__main__":
+    main()
