577e77034e58ca5d39cbe95abba6b4f9d47fa18c
Ryuya Ikeda <rikeda71@gmail.com>
2017-09-26 11:03:47 +0900

    q82をメモリ不足に対応した形に変更

--- 09/q82.py
+++ 09/q82.py
@@ -5,30 +5,23 @@ import gc
 
 
 def main():
-    with open("tokens_81.txt", "r") as f:
-        sentences = [sentence.replace("\n", "").split(" ") for sentence in f.readlines()]
-    # 文脈の抽出
-    context = []
-    for sentence in sentences:
-        sentence = [word for word in sentence if word != ""]
-        for i in range(len(sentence)):
-            rand = random.randint(1, 5)
-            start = i - rand if i - rand >= 0 else 0
-            end = i + rand + 1 if i + rand + 1 < len(sentence) else len(sentence)
-            context.extend([sentence[i] + "\t" + sentence[j] for j in range(start, end) if i != j])
-    text = list(set(context))
-
-    # メモリ解放
-    del sentence
-    del sentences
-    del context
-    del rand
-    gc.collect()
-
-    with open("contexts.txt", "a") as f:
-        for sentence in text:
-            print(sentence)
-            f.write(sentence)
+    sentences = []
+    with open("tokens_81.txt", "r") as rf:
+        for sentence in rf.readlines():
+            context = []
+            words = [word for word in sentence.replace("\n", " ").split(" ") if word != ""]
+            if len(words) < 2:
+                continue
+            # 文脈の抽出
+            for i in range(len(words)):
+                rand = random.randint(1, 5)
+                start = i - rand if i - rand >= 0 else 0
+                end = i + rand + 1 if i + rand + 1 < len(words) else len(words)
+                context.extend([words[i] + "\t" + words[j] for j in range(start, end) if i != j])
+            # 書き込み
+            text = "\n".join(list(set(context)))
+            with open("contexts.txt", "a") as wf:
+                wf.write(text)
 
 
 if __name__ == "__main__":
