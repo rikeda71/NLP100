1ba88fc4dbb47915bd716490d3e024b265f08300
Ryuya Ikeda <rikeda71@gmail.com>
2017-09-27 15:21:14 +0900

    fix algorithm

--- 09/q80.py
+++ 09/q80.py
@@ -3,18 +3,23 @@ import re
 
 
 def main():
-    tokens = []
+    new_sentences = ""
+    r = re.compile(r"[.,!\?;:\(\)\[\]\'\"]")
+    # ファイル読み込み 後の処理のために改行コードの前に空白を入れる
     with open("enwiki-20150112-400-r10-105752.txt", "r") as f:
-        sentences = [sentence for sentence in f.readlines()]
+        sentences = [sentence[:-1] for sentence in f.readlines()]
     # トークンごとに分割
     for sentence in sentences:
-        tokens.extend(sentence.split(" "))
-    # いらない文字を削除
-    new_sentences = ""
-    for token in tokens:
-        true_token = re.sub(r"[.,!\?;:\(\)\[\]\'\"]", "", token)
-        if true_token != "":
-            new_sentences += true_token + " "
+        tokens = sentence.split(" ")
+        # いらない文字を削除
+        for token in tokens:
+            if len(token) >= 3:
+                true_token = r.sub("", token[0]) + token[1:-1] + r.sub("", token[-1])
+            else:
+                true_token = r.sub("", token)
+            if true_token != "":
+                new_sentences += true_token + " "
+        new_sentences = new_sentences[:-1] + "\n"
     # メモリ解放
     del sentences
     del tokens
