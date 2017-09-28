06df6a26f3cbbe4788922e6062043a4b16928cf0
Ryuya Ikeda <rikeda71@gmail.com>
2017-09-28 19:17:27 +0900

    fix q80

--- 09/q80.py
+++ 09/q80.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 import re
+import gc
 
 
 def main():
     new_sentences = ""
-    r = re.compile(r"[.,!\?;:\(\)\[\]\'\"]")
+    r = re.compile(r"([.,!\?;:\(\)\[\]\'\"])")
     # ファイル読み込み 後の処理のために改行コードの前に空白を入れる
     with open("enwiki-20150112-400-r10-105752.txt", "r") as f:
         sentences = [sentence[:-1] for sentence in f.readlines()]
     # トークンごとに分割
+    i = 0
     for sentence in sentences:
+        i += 1
+        print(i)
         tokens = sentence.split(" ")
         # いらない文字を削除
         for token in tokens:
-            if len(token) >= 3:
-                true_token = r.sub("", token[0]) + token[1:-1] + r.sub("", token[-1])
-            else:
-                true_token = r.sub("", token)
+            true_token = r.sub("", token)
             if true_token != "":
                 new_sentences += true_token + " "
         new_sentences = new_sentences[:-1] + "\n"
     # メモリ解放
     del sentences
     del tokens
+    gc.collect()
 
     with open("tokens.txt", "w") as f:
         f.write(new_sentences)
