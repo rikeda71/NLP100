96aa160be91ad0d2ec888ef58b8ba4262ecf7768
Ryuya Ikeda <rikeda71@gmail.com>
2018-03-17 10:40:09 +0900

    Edit: 84番の問題で，テキストではなくscipy行列を保存するように変更

--- 09/q84.py
+++ 09/q84.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from math import log
+from scipy import io, sparse
 
 
 def PPMI(N: float, f_t_c: float, f_t_a: float, f_a_c: float) -> float:
@@ -26,7 +27,6 @@ def get_word_count_dict(path: str) -> dict:
 
 def main():
     cooc_dic = {}
-    matrix = {}
     # 値を保持する共起だけ抽出
     with open("count/cooc.txt", "r") as f:
         line = f.readline()
@@ -42,17 +42,35 @@ def main():
     with open("count/N.txt", "r") as f:
         N = int(f.readline())
 
+    # t:単語, c:文脈語
+    t = []
+    c = []
+    for k in cooc_dic.keys():
+        split = k.split("\t")
+        t.append(split[0])
+        c.append(split[1])
+    t = sorted(list(set(t)))
+    c = sorted(list(set(c)))
+
+    """
+    # 疎行列の用意
+    a = sparse.lil_matrix((len(t), len(c)))
     # PPMIの計算
     for k, v in cooc_dic.items():
-        split = k.split("\t")
-        matrix[k] = PPMI(N, v, word_dic[split[0]], cont_dic[split[1]])
+        s = k.split("\t")
+        i = t.index(s[0]) if s[0] in t else -1
+        j = c.index(s[1]) if s[1] in c else -1
+        if i < 0 or j < 0:
+            continue
+        a[i, j] = PPMI(N, v, word_dic[s[0]], cont_dic[s[1]])
+    """
 
     # 書き込み
-    text = ""
-    for k, v in matrix.items():
-        text += k + " " + str(v) + "\n"
-    with open("matrix.txt", "w") as f:
-        f.write(text)
+    # io.savemat("matrix_t_to_c", {"a": a})
+    with open("matrix_x.txt", "w") as f:
+        f.write("\n".join(t))
+    with open("matrix_y.txt", "w") as f:
+        f.write("\n".join(c))
 
 
 if __name__ == "__main__":
